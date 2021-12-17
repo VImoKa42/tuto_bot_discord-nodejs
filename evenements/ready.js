708c288200c3cc4c9c58f981cd4acb5a456ca167
VImoKa42 <96071691+VImoKa42@users.noreply.github.com>
2021-12-17 23:43:43 +0100

    Update ready.js

--- evenements/ready.js
+++ evenements/ready.js
@@ -1,7 +1,7 @@
-let fonctions = require("../fonctions/log.js");
+//let fonctions = require("../fonctions/log.js");
 let { prefix } = require("../parametres.json");
 
 module.exports = (client) => {
     console.log("Le bot est connecté");
-    fonctions.message_log("Connection du BOT",`Le BOT tuto BOT est connecté.\nprefix: ${prefix}`,"blue",client);
+    //fonctions.message_log("Connection du BOT",`Le BOT tuto BOT est connecté.\nprefix: ${prefix}`,"blue",client);
 }
